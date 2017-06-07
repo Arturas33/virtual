2b888b698f6c3b2de18e5e5b4a687b70075a3105
Arturas33 <jasinskis330@gmail.com>
2017-06-07 13:39:01 +0300

    controllers

---
+++ app/Http/Controllers/VrUsersController.php
@@ -0,0 +1,107 @@
+<?php namespace App\Http\Controllers;
+
+use App\Models\VrOrder;
+use App\Models\VrUsers;
+use Illuminate\Routing\Controller;
+use Ramsey\Uuid\Uuid;
+
+class VrUsersController extends Controller
+{
+
+    /**
+     * Display a listing of the resource.
+     * GET /vrusers
+     *
+     * @return Response
+     */
+    public function index()
+    {
+       //
+    }
+
+    public function orderIndex (string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     * GET /vrusers/create
+     *
+     * @return Response
+     */
+    public function create()
+    {
+       //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * POST /vrusers
+     *
+     * @return Response
+     */
+    public function store()
+    {
+      //
+    }
+
+    /**
+     * Display the specified resource.
+     * GET /vrusers/{id}
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function show($id)
+    {
+       //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     * GET /vrusers/{id}/edit
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+       //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     * PUT /vrusers/{id}
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function update($id)
+    {
+       //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * DELETE /vrusers/{id}
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    private function listBladeData()
+    {
+        $config = [];
+        $config['show'] = 'app.users.show';
+        $config['create'] = 'app.users.create';
+        $config['delete'] = 'app.users.destroy';
+        $config['edit'] = 'app.users.edit';
+        return $config;
+    }
+
+}
