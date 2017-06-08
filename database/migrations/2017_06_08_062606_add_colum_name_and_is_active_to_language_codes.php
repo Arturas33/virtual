1b1c6067be402b97b361523283b8261b4ab74b6d
Arturas33 <jasinskis330@gmail.com>
2017-06-08 09:46:05 +0300

    add helper dump

---
+++ database/migrations/2017_06_08_062606_add_colum_name_and_is_active_to_language_codes.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumNameAndIsActiveToLanguageCodes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('vr_language_codes', function (Blueprint $table) {
+            $table->string('name', 255);
+            $table->tinyInteger('is_active');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('vr_language_codes', function (Blueprint $table) {
+            $table->dropColumn('name');
+            $table->dropColumn('is_active');
+        });
+    }
+}
