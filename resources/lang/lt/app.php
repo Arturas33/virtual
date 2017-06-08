94650d4ff9ea03222d2401cc5a3e7e13d22489ef
Arturas33 <jasinskis330@gmail.com>
2017-06-08 13:33:03 +0300

    multi language

---
+++ resources/lang/lt/app.php
@@ -0,0 +1,13 @@
+<?php
+
+
+return [
+
+    'language' => 'Kalba',
+    'pages' => 'Puslapiai',
+    'categories' => 'Kategorjos',
+    'menu' => 'Meniu',
+    'orders' => 'Užsakimai',
+    'user' => 'Vartotojai'
+
+];
