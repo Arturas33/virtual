6ee94664721db1920989eeafdefa77966ffc91c7
Arturas33 <jasinskis330@gmail.com>
2017-06-09 13:25:37 +0300

    requesting lang code data from DB

--- app/Http/Controllers/VrLanguageCodesController.php
+++ app/Http/Controllers/VrLanguageCodesController.php
@@ -3,93 +3,99 @@
 use App\Models\VrLanguageCodes;
 use Illuminate\Routing\Controller;
 
-class VrLanguageCodesController extends Controller {
+class VrLanguageCodesController extends Controller
+{
 
-	/**
-	 * Display a listing of the resource.
-	 * GET /vrlanguagecodes
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		$config = [];
-		$config['list'] = VrLanguageCodes::get()->toArray();
-		$config['callToAction'] = 'app.language.edit';
+    /**
+     * Display a listing of the resource.
+     * GET /vrlanguagecodes
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        $config = [];
+        $config['list'] = VrLanguageCodes::get()->toArray();
+        $config['callAction'] = 'app.language.edit';
 
         return view('admin.list', $config);
 
 
-	}
+    }
 
-	/**
-	 * Show the form for creating a new resource.
-	 * GET /vrlanguagecodes/create
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
+    /**
+     * Show the form for creating a new resource.
+     * GET /vrlanguagecodes/create
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
 
-	/**
-	 * Store a newly created resource in storage.
-	 * POST /vrlanguagecodes
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
+    /**
+     * Store a newly created resource in storage.
+     * POST /vrlanguagecodes
+     *
+     * @return Response
+     */
+    public function store()
+    {
+        //
+    }
 
-	/**
-	 * Display the specified resource.
-	 * GET /vrlanguagecodes/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
+    /**
+     * Display the specified resource.
+     * GET /vrlanguagecodes/{id}
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        //
+    }
 
-	/**
-	 * Show the form for editing the specified resource.
-	 * GET /vrlanguagecodes/{id}/edit
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
+    /**
+     * Show the form for editing the specified resource.
+     * GET /vrlanguagecodes/{id}/edit
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        //
+    }
 
-	/**
-	 * Update the specified resource in storage.
-	 * PUT /vrlanguagecodes/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
+    /**
+     * Update the specified resource in storage.
+     * PUT /vrlanguagecodes/{id}
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function update($id)
+    {
+        $data = request()->all();
 
-	/**
-	 * Remove the specified resource from storage.
-	 * DELETE /vrlanguagecodes/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
+        $record = VrLanguageCodes::find($id);
+        $record->update($data);
+
+        return $record;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * DELETE /vrlanguagecodes/{id}
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 
 }
