ca55396d7b9fb57e4c44b6f7abf3ba63850e0e37
Arturas33 <jasinskis330@gmail.com>
2017-06-12 15:44:41 +0300

    new translations

--- resources/lang/lt/app.php
+++ resources/lang/lt/app.php
@@ -12,6 +12,10 @@ return [
     'no_data' => 'Nera duomenu',
     'active' => 'ijungti',
     'disable' => 'išjungti',
-    'language_codes_list' => 'Kalbų sąrašas'
+    'language_codes_list' => 'Kalbų sąrašas',
+     'category_list' => 'Kategorijų sąrašas',
+     'orders_list' => 'Užsakymų sąrašas',
+     'pages_list' => 'Puslapių sąrašas',
+     'users_list' => 'Vartotojų sąrašas',
 
 ];
