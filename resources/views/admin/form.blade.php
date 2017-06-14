4a6fb7cc6e16273948a945a9c45e8b05b4901e9d
Arturas33 <jasinskis330@gmail.com>
2017-06-14 09:49:41 +0300

    added new fields in form

--- resources/views/admin/form.blade.php
+++ resources/views/admin/form.blade.php
@@ -1,32 +1,42 @@
-
-
 @extends('admin.core')
 @section('content')
 
-<div id="list">
+    <div id="list">
+
+
+        {!! Form::open(['url' => $route])!!}
+
+        @foreach($fields as $field)
+            @if($field['type']== 'drop_down')
+                {{ Form::label(trans('app.select') )}}
+                {{Form::select($field['key'],$field ['options'] )}}
+                <br>
+            @elseif($field['type'] == 'single_line')
+
+
+                {{ Form::label($field['key'], trans('app.' . $field['key'])) }}
+                {{Form::text($field['key'])}}
+                <br>
+
+            @elseif($field['type'] == 'check_box')
+
+
+                {{ Form::label($field['key'], trans('app.' . $field['key'])) }}
+                {{Form::checkbox('key', 'value')}}
+                <br>
+
+
+
+            @endif
+
+        @endforeach
 
 
 
 
-    {!! Form::open(['url' => $route])!!}
 
-    @foreach($fields as $field)
-        @if($field['type']== 'drop_down')
-            {{Form::select($field['key'],$field ['options'] )}}
-        @else
-
-            {{Form::text($field['key'])}}
-
-        @endif
-
-    @endforeach
-
-
-
-
-
-    {{ Form::submit('Create') }}
-    {!! Form::close() !!}
-</div>
+        {{ Form::submit('Create') }}
+        {!! Form::close() !!}
+    </div>
 
 @endsection
