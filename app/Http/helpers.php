3cea9e9fe64c04b2727cb4536639b02784820b2c
Arturas33 <jasinskis330@gmail.com>
2017-06-15 10:43:59 +0300

    added function check locale helpers

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -4,6 +4,21 @@ use App\Models\VrLanguageCodes;
 
 function getActiveLanguages()
 {
-    $config = VrLanguageCodes::where('is_active', '1')->pluck('name', 'id')->toArray();
+    $language = VrLanguageCodes::where('is_active', '1')->pluck('name', 'id')->toArray();
+
+    $locale = app()->getLocale();
+
+    if (!isset($language[$locale]))
+    {
+        $locale = config('app.fallback_locale');
+
+        if (!isset($language[$locale]))
+        {
+            return $language;
+        }
+    }
+
+    $config = array($locale => $language[$locale]) + $language;
+
     return $config;
 }
