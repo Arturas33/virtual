de78a6998f7c628cb92b47fcd015aeb00fbc5ec5
Arturas33 <jasinskis330@gmail.com>
2017-06-16 15:25:42 +0300

    added function in pages model

--- app/Models/VrPages.php
+++ app/Models/VrPages.php
@@ -20,4 +20,18 @@ class VrPages extends CoreModel
     protected $fillable = ['id', 'category_id', 'cover_id'];
 
 
+    protected $with = ['translations'];
+
+    public function translations()
+    {
+
+        $language = request('language_code');
+        if($language == null)
+            $language = app()->getLocale();
+
+        return $this->hasOne(VrPagesTranslations::class, 'record_id', 'id')->where('language_code', $language);
+      //  return $this->hasMany(VrCategories::class, 'record_id', 'id')->where('language_code', $language);
+    }
+
+
 }
