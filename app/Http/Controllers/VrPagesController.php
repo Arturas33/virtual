fe26a6603f107f2a9c6f4a834ac183e80299aaac
Arturas33 <jasinskis330@gmail.com>
2017-06-19 11:46:51 +0300

    added edit function

--- app/Http/Controllers/VrPagesController.php
+++ app/Http/Controllers/VrPagesController.php
@@ -7,112 +7,133 @@ use App\Models\VrPagesTranslations;
 use App\Models\VrResources;
 use Illuminate\Routing\Controller;
 
-class VrPagesController extends Controller {
+class VrPagesController extends Controller
+{
 
-	/**
-	 * Display a listing of the resource.
-	 * GET /vrpages
-	 *
-	 * @return Response
-	 */
+    /**
+     * Display a listing of the resource.
+     * GET /vrpages
+     *
+     * @return Response
+     */
 
-	public function index()
-	{
-       $config['list'] = VrPages::get()->toArray();
-       $config['title'] = trans('app.adminPages');
-       $config['route'] = route('app.pages.create');
-       $config['create'] = 'app.pages.create';
+    public function index()
+    {
+        $config['list'] = VrPages::get()->toArray();
+        $config['title'] = trans('app.adminPages');
+        $config['route'] = route('app.pages.create');
+        $config['create'] = 'app.pages.create';
 
-       $config['edit'] = 'app.pages.edit';
-       $config['delete'] = 'app.pages.destroy';
+        $config['edit'] = 'app.pages.edit';
+        $config['delete'] = 'app.pages.destroy';
 
-       //dd($config);
+        //dd($config);
 
-       return view('admin.list', $config);
+        return view('admin.list', $config);
 
-	}
-
-	/**
-	 * Show the form for creating a new resource.
-	 * GET /vrpages/create
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		$config = $this -> getFormData();
-		$config['route'] = route('app.pages.create');
-		$config['back'] = 'app.pages.index';
-
-		//dd($config);
-
-        return view('admin.form', $config);
-	}
-
-	/**
-	 * Store a newly created resource in storage.
-	 * POST /vrpages
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-      $data = request()->all();
-      $record = VrPages::create($data);
-      $data['record_id'] = $record->id;
-      VrPagesTranslations::create($data);
-
-
-      return redirect(route('app.pages.edit', $record->id ));
     }
 
-	/**
-	 * Display the specified resource.
-	 * GET /vrpages/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-       //
-	}
+    /**
+     * Show the form for creating a new resource.
+     * GET /vrpages/create
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        $config = $this->getFormData();
+        $config['route'] = route('app.pages.create');
+        $config['back'] = 'app.pages.index';
 
-	/**
-	 * Show the form for editing the specified resource.
-	 * GET /vrpages/{id}/edit
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-       //
-	}
+        //dd($config);
 
-	/**
-	 * Update the specified resource in storage.
-	 * PUT /vrpages/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-	   //
-	}
+        return view('admin.form', $config);
+    }
 
-	/**
-	 * Remove the specified resource from storage.
-	 * DELETE /vrpages/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
+    /**
+     * Store a newly created resource in storage.
+     * POST /vrpages
+     *
+     * @return Response
+     */
+    public function store()
+    {
+        $data = request()->all();
+        $resorces = request()->file('file');
+        $uploadController = new VrResourcesController();
+        $record = $uploadController->upload($resorces);
+        $data['cover_id'] = $record->id;
+        $record = VrPages::create($data);
+        $data['record_id'] = $record->id;
+        VrPagesTranslations::create($data);
+
+
+        return redirect(route('app.pages.edit', $record->id));
+    }
+
+    /**
+     * Display the specified resource.
+     * GET /vrpages/{id}
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function show($id)
+    {
         //
-	}
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     * GET /vrpages/{id}/edit
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+
+        $record = VrPages::find($id)->toArray();
+        $record['slug'] = $record['translations']['slug'];
+        $record['title'] = $record['translations']['title'];
+        $record['language_code'] = $record['translations']['language_code'];
+        $record['description_short'] = $record['translations']['description_short'];
+        $record['description_long'] = $record ['translations']['description_long'];
+
+        $config = $this->getFormData();
+        $config['record'] = $record;
+        $config['titleForm'] = $id;
+        $config['route'] = route('app.pages.create');
+        $config['back'] = 'app.pages.index';
+       //dd($config);
+        return view('admin.form', $config);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     * PUT /vrpages/{id}
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function update($id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * DELETE /vrpages/{id}
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        VrPagesTranslations::destroy(VrPagesTranslations::where('record_id', $id)->pluck('id')->toArray());
+        VrPages::destroy($id);
+        return ["success" => true, "id" => $id];
+    }
 
     private function getFormData()
     {
@@ -122,6 +143,11 @@ class VrPagesController extends Controller {
             $language = app()->getLocale();
 
 
+        $config['fields'][] = [
+            "type" => "file",
+            "key" => "cover_id",
+            "file" => VrResources::pluck('path', 'id')->toArray()
+        ];
         $config['fields'][] = [
             'type' => 'drop_down',
             'key' => 'language_code',
@@ -149,7 +175,7 @@ class VrPagesController extends Controller {
             "type" => "drop_down",
             "key" => "category_id",
             "label" => trans('app.adminCategory'),
-            "options" => VrCategoriesTranslations::where('language_code', $language)->pluck('name', 'record_id'),
+            "options" => VrCategoriesTranslations::where('language_code', $language)->pluck('name', 'record_id')->toArray(),
         ];
         return $config;
     }
