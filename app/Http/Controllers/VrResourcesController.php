d9480c29fa18ca017f1e7d12cb251bc273c0d69a
Arturas33 <jasinskis330@gmail.com>
2017-06-21 15:51:51 +0300

    fixed upload function

--- app/Http/Controllers/VrResourcesController.php
+++ app/Http/Controllers/VrResourcesController.php
@@ -5,102 +5,105 @@ use Carbon\Carbon;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Routing\Controller;
 
-class VrResourcesController extends Controller {
+class VrResourcesController extends Controller
+{
 
     public function upload(UploadedFile $file)
     {
         $data =
             [
                 "size" => $file->getsize(),
-                "mime_type"=>$file->getMimetype(),
+                "mime_type" => $file->getMimetype(),
             ];
 
-        $path = 'upload/' . date ("Y/m/d/");
-        $fileName = Carbon::now()->timestamp . '-' .$file->getClientOriginalName();
+
+        $path = 'upload/' . date("Y/m/d/");
+        $fileName = Carbon::now()->timestamp . '-' . $file->getClientOriginalName();
         $file->move(public_path($path), $fileName);
         $data["path"] = $path . $fileName;
 
         return VrResources::create($data);
     }
-	/**
-	 * Display a listing of the resource.
-	 * GET /vrresources
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		//
-	}
 
-	/**
-	 * Show the form for creating a new resource.
-	 * GET /vrresources/create
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
+    /**
+     * Display a listing of the resource.
+     * GET /vrresources
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        //
+    }
 
-	/**
-	 * Store a newly created resource in storage.
-	 * POST /vrresources
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-		//
-	}
+    /**
+     * Show the form for creating a new resource.
+     * GET /vrresources/create
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
 
-	/**
-	 * Display the specified resource.
-	 * GET /vrresources/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-		//
-	}
+    /**
+     * Store a newly created resource in storage.
+     * POST /vrresources
+     *
+     * @return Response
+     */
+    public function store()
+    {
+        //
+    }
 
-	/**
-	 * Show the form for editing the specified resource.
-	 * GET /vrresources/{id}/edit
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
+    /**
+     * Display the specified resource.
+     * GET /vrresources/{id}
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        //
+    }
 
-	/**
-	 * Update the specified resource in storage.
-	 * PUT /vrresources/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
+    /**
+     * Show the form for editing the specified resource.
+     * GET /vrresources/{id}/edit
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        //
+    }
 
-	/**
-	 * Remove the specified resource from storage.
-	 * DELETE /vrresources/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
+    /**
+     * Update the specified resource in storage.
+     * PUT /vrresources/{id}
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function update($id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * DELETE /vrresources/{id}
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 
 }
