c1d60feab5a86581762c68411eab0172567bb01d
Arturas33 <jasinskis330@gmail.com>
2017-06-22 14:30:07 +0300

    create menu blade

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,5 +1,51 @@
-<html>
+{{--{{dd($vr_room)}}--}}
 
-{!! dd($menu) !!}
+<div>
+    <ul class="nav navbar-nav">
+        {{--{{dd($menu)}}--}}
+        @foreach( $menu as $record)
 
-</html>
+            @if (sizeof($record['sub_menu'])>0)
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+                       aria-expanded="false">{{($record['translations']['name'])}} <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        @foreach($record['sub_menu'] as $element)
+                            <li><a href="{{$element['translations']['url']}}">{{$element['translations']['name']}}</a>
+                            </li>
+                        @endforeach
+                    </ul>
+            @else
+                <li class="active"><a href="{{$record['translations']['url']}}"> {{$record['translations']['name']}}
+                        <span class="sr-only">(current)</span></a></li>
+                </li>
+            @endif
+        @endforeach
+
+        <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+               aria-expanded="false">{{trans('app.language')}} <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+                @foreach($lang_menu as $key => $value)
+                    <li><a href="{{$key}}">{{$value}}</a></li>
+                @endforeach
+            </ul>
+        </li>
+
+        <li class="dropdown">
+            <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true"
+               aria-expanded="false">{{trans('app.room')}} <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+                @foreach($vr_room as $value)
+                    <li>
+                        <a href=" {{app()->getLocale(). '/pages/' .($value['translations']['slug'])}}">{{$value['translations']['title']}}</a>
+                    </li>
+                @endforeach
+            </ul>
+        </li>
+
+
+    </ul>
+</div>
+
+
