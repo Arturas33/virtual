6fef1e7a76e05d41820e3cdcf1f52aa8f8b185fc
Arturas33 <jasinskis330@gmail.com>
2017-06-23 15:29:24 +0300

    orders service

--- app/Http/Controllers/VrOrderController.php
+++ app/Http/Controllers/VrOrderController.php
@@ -3,100 +3,160 @@
 use App\Models\VrOrder;
 use App\Models\VrPages;
 use App\Models\VrReservations;
+use App\Models\VrUsers;
 use Carbon\Carbon;
 use DateTimeZone;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Input;
 
-class VrOrderController extends Controller {
+class VrOrderController extends Controller
+{
 
-	/**
-	 * Display a listing of the resource.
-	 * GET /vrorder
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-        $config['title'] = trans('app.orders_list');
-	}
+    /**
+     * Display a listing of the resource.
+     * GET /vrorder
+     *
+     * @return Response
+     */
+    public function index()
+    {
+
+        $config['list'] = VrOrder::get()->toArray();
+        $config['title'] = trans('app.adminOrder');
+        $config['route'] = route('app.orders.create');
+        $config['create'] = 'app.orders.create';
+
+        $config['show'] = 'app.orders.show';
+        $config['edit'] = 'app.orders.edit';
+        $config['delete'] = 'app.orders.destroy';
 
 
-	/**
-	 * Show the form for creating a new resource.
-	 * GET /vrorder/create
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-
-		//
-
-	}
-
-	/**
-	 * Store a newly created resource in storage.
-	 * POST /vrorder
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
-       //
+        // dd($config);
+        return view('admin.list', $config);
 
     }
 
-	/**
-	 * Display the specified resource.
-	 * GET /vrorder/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
+
+    /**
+     * Show the form for creating a new resource.
+     * GET /vrorder/create
+     *
+     * @return Response
+     */
+    public function create()
+    {
+
+        $config = $this->getFormData();
+        $config['route'] = route('app.orders.create');
+        $config['back'] = 'app.orders.index';
+//        dd($config);
+
+
+        return view('admin.form', $config);
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * POST /vrorder
+     *
+     * @return Response
+     */
+    public function store()
+    {
+        $data = request()->all();
+        VrOrder::create($data);
+
+
+        // dd($record);
+
+        return redirect(route('app.orders.index'));
+
+    }
+
+    /**
+     * Display the specified resource.
+     * GET /vrorder/{id}
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function show($id)
+    {
+
         //
-	}
+    }
 
-	/**
-	 * Show the form for editing the specified resource.
-	 * GET /vrorder/{id}/edit
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
+    /**
+     * Show the form for editing the specified resource.
+     * GET /vrorder/{id}/edit
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        $record = VrOrder::find($id)->toArray();
+        $config = $this->getFormData();
+        $config['record'] = $record;
+        $config['titleForm']= $id;
+        $config['route'] = route('app.orders.edit', $id);
+        $config['back'] = 'app.orders.index';
 
-	/**
-	 * Update the specified resource in storage.
-	 * PUT /vrorder/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
+        return view('admin.form', $config);
+    }
 
-	/**
-	 * Remove the specified resource from storage.
-	 * DELETE /vrorder/{id}
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
+    /**
+     * Update the specified resource in storage.
+     * PUT /vrorder/{id}
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function update($id)
+    {
+        $data = request()->all();
+        $record = VrOrder::find($id);
+        $record->update($data);
 
-    private function listBladeData()
+
+        return redirect(route('app.orders.index'));
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * DELETE /vrorder/{id}
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function destroy($id)
     {
         //
+    }
+
+    private function getFormData()
+    {
+
+
+        $config['fields'][] = [
+            'type' => 'drop_down',
+            'key' => 'user_id',
+            'options' => VrUsers::pluck('email', 'id')->toArray(),
+        ];
+
+
+        $config['fields'][] = [
+            "type" => "drop_down",
+            "key" => "status",
+            'options' => [
+                'pending'=> trans('app.pending'),
+                'canceled'=> trans('app.canceled'),
+                'aproved'=>trans('app.aproved') ,
+            ],
+
+        ];
+
+        return $config;
     }
 }
