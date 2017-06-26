fe4d1b00a01cb3847a4d13b336b6c4c700c4f2bc
Arturas33 <jasinskis330@gmail.com>
2017-06-26 14:35:08 +0300

    added drop down time and rooms

--- app/Http/Controllers/VrOrderController.php
+++ app/Http/Controllers/VrOrderController.php
@@ -2,6 +2,7 @@
 
 use App\Models\VrOrder;
 use App\Models\VrPages;
+use App\Models\VrPagesTranslations;
 use App\Models\VrReservations;
 use App\Models\VrUsers;
 use Carbon\Carbon;
@@ -99,7 +100,7 @@ class VrOrderController extends Controller
         $record = VrOrder::find($id)->toArray();
         $config = $this->getFormData();
         $config['record'] = $record;
-        $config['titleForm']= $id;
+        $config['titleForm'] = $id;
         $config['route'] = route('app.orders.edit', $id);
         $config['back'] = 'app.orders.index';
 
@@ -135,24 +136,53 @@ class VrOrderController extends Controller
         //
     }
 
+
+    private function getVRroomsWithcategory()
+    {
+        $pagesData = (new VrPages)->getTable();
+        $pagesDataTrans = (new VrPagesTranslations)->getTable();
+
+        return (VrPages::where('category_id', 'vr_rooms')->join($pagesDataTrans, "$pagesDataTrans.record_id", '=', "$pagesData.id")
+            ->pluck("$pagesDataTrans.title", "$pagesData.id")->toArray());
+
+    }
+
     private function getFormData()
     {
 
+        $options = [];
+        $now = Carbon::now();
+        $end_data = Carbon::createFromDate()->addDay(14);
+        for ($option = $now; $option->lte($end_data); $option->addDay()) {
+            $options[$option->format('Y-m-d')] = $option->format('Y-m-d');
+        }
+
+
+        $config['fields'][] = [
+            "type" => "drop_down",
+            "key" => "time",
+            "options" => $options
+        ];
+
+        $config['fields'][] = [
+            "type" => "drop_down",
+            "key" => "virtual_room",
+            "options" => $this->getVRroomsWithcategory()
+        ];
 
         $config['fields'][] = [
             'type' => 'drop_down',
             'key' => 'user_id',
             'options' => VrUsers::pluck('email', 'id')->toArray(),
         ];
-
-
+        
         $config['fields'][] = [
             "type" => "drop_down",
             "key" => "status",
             'options' => [
-                'pending'=> trans('app.pending'),
-                'canceled'=> trans('app.canceled'),
-                'aproved'=>trans('app.aproved') ,
+                'pending' => trans('app.pending'),
+                'canceled' => trans('app.canceled'),
+                'aproved' => trans('app.aproved'),
             ],
 
         ];
